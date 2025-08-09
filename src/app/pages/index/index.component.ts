855552f33d8cb16faf45c24782b01e248ce71015
sameer <sameerkhan9991303458@gmail.com>
2025-08-09 22:30:47 +0530

    abc

---
+++ src/app/pages/index/index.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from '../../components/header/header.component';
+
+@Component({
+  selector: 'app-index',
+  imports: [HeaderComponent],
+  templateUrl: './index.component.html',
+  styleUrl: './index.component.scss'
+})
+export class IndexComponent {
+
+}
+
