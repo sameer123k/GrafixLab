7161da9599fed17568cda5b61324ffae0097e6db
sameer <sameerkhan9991303458@gmail.com>
2025-08-10 21:55:42 +0530

    abc

--- src/app/pages/index/index.component.ts
+++ src/app/pages/index/index.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
 import { HeroComponent } from '../../components/hero/hero.component';
+import { FooterComponent } from '../../components/footer/footer/footer.component';
 
 @Component({
   selector: 'app-index',
-  imports: [HeaderComponent, HeroComponent],
+  imports: [HeaderComponent, HeroComponent,FooterComponent],
   templateUrl: './index.component.html',
   styleUrl: './index.component.scss'
 })
