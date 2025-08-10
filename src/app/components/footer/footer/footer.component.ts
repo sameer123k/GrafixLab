8a4f783f340d2eae5eedcde4735c6441c5c202bc
sameer <sameerkhan9991303458@gmail.com>
2025-08-10 21:36:54 +0530

    abc

---
+++ src/app/components/footer/footer/footer.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  imports: [],
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.scss'
+})
+export class FooterComponent {
+
+  currentYear: number;
+
+  constructor() {
+    this.currentYear = new Date().getFullYear();
+  }
+
+}
