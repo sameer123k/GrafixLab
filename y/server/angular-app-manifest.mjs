ff7178b9cff02873e12317a68f9b35760a18415b
sameer <sameerkhan9991303458@gmail.com>
2025-08-27 23:23:13 +0530

    abc

--- y/server/angular-app-manifest.mjs
+++ y/server/angular-app-manifest.mjs
@@ -25,11 +25,11 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 5810, hash: '6cb9f9df501c37d17ee614b0e28555411aa7737cea6ac47a7146af3f72ede161', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1782, hash: 'c8f1dab7082d1826ecba5ea654b516dd6be9b14470ad473e49ccf72e0c4d09a0', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'colorpallete/index.html': {size: 20660, hash: 'c6fa0a043fe90d7417729ccd6e8f64a759c61e9877662a3f591f25d82faa3a68', text: () => import('./assets-chunks/colorpallete_index_html.mjs').then(m => m.default)},
-    'index.html': {size: 21316, hash: '488999c2d07578c94ef499c3572dfae589f6b54e4e1704e6aee40312851dfdc1', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
-    'image-format-changer/index.html': {size: 20597, hash: 'd41bddc9106c76eeefad0e81300b27ac9b9a6aaa1b865920962496064555fd9d', text: () => import('./assets-chunks/image-format-changer_index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 5690, hash: '37ca19b8feae50b7cf19f7f79d8a12eea4936e541cb70e29064d000cfeebb665', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1662, hash: '46f45d226332be799b1aa9f80688a2821eb187ee932c45b2fba84a56c63b4d60', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'colorpallete/index.html': {size: 20540, hash: '086c9635d890b8a7b4f8793cd0c4f9dd5b90a0b12d6ed8fb5b9b083b4c480fcf', text: () => import('./assets-chunks/colorpallete_index_html.mjs').then(m => m.default)},
+    'index.html': {size: 21196, hash: 'a9f720d01769470457971bf844502f81da196dfed2fb78a8aaca1be3a9ed5ef8', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'image-format-changer/index.html': {size: 20477, hash: '4e6525f81a8e6b2a08671ea42130f5f407f17ae9e278517549bbf982659c819c', text: () => import('./assets-chunks/image-format-changer_index_html.mjs').then(m => m.default)},
     'styles-SDBZV4GP.css': {size: 230949, hash: 'Ph+9IYpH6Nk', text: () => import('./assets-chunks/styles-SDBZV4GP_css.mjs').then(m => m.default)}
   },
 };
